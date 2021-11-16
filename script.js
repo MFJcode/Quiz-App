b37cba686b97c9c6652749bd041105a936329454
MFJcode <mfjcode@gmail.com>
2021-11-16 15:51:19 +0100

    getElement

--- script.js
+++ script.js
@@ -53,3 +53,18 @@ const quizData = [
 
 ];
 
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let currentQuestion = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+
+
+
+    currentQuestion++;
+}
