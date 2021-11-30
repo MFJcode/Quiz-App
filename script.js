7db4ac64034bac65cae3e32e2271af16b8a01819
MFJcode <mfjcode@gmail.com>
2021-11-30 23:25:19 +0100

    Reload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const quizData = [
         b: '4,034 billion years',
         c: '3,580 billion years',
         d: '2,943 billion years',
-        correct: 'a'
+        correct: 'a',
     },
 
     {
@@ -15,7 +15,7 @@ const quizData = [
         b: 'C',
         c: 'Python',
         d: 'Javascript',
-        correct: 'd'
+        correct: 'd',
     },
 
     {
@@ -24,7 +24,7 @@ const quizData = [
         b: 'Cascading Style Sheet',
         c: 'Jason Object Notation',
         d: 'Helicopters Terminals Motorboats Lamborginis',
-        correct: 'a'
+        correct: 'a',
 
     },
 
@@ -35,7 +35,7 @@ const quizData = [
         b: '1995',
         c: '1994',
         d: 'none of the above',
-        correct: 'b'
+        correct: 'b',
 
     },
 
@@ -45,7 +45,7 @@ const quizData = [
         b: '1996',
         c: '2018',
         d: 'none of the above',
-        correct: 'b'
+        correct: 'b',
 
     },
 
@@ -53,16 +53,15 @@ const quizData = [
 
 ];
 
-const answersEls = document.querySelectorAll(".answer");
 const quiz = document.getElementById("quiz");
+const answersEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
 
-const questionEl = document.getElementById('question');
-
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
 
 let currentQuiz = 0;
@@ -73,6 +72,7 @@ loadQuiz();
 
 function loadQuiz() {
     deselectAnswers();
+
     const currentQuizData = quizData [currentQuiz];
 
     questionEl.innerHTML = currentQuizData.question;
@@ -102,7 +102,7 @@ function deselectAnswers() {
   });
 }
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener("click", () => {
     // check to see the answer
     const answer = getSelected();
 
@@ -116,7 +116,7 @@ submitBtn.addEventListener('click', () => {
         if (currentQuiz < quizData.length) {
             loadQuiz();
         } else{
-            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} question</h2>`;
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2> <button onclick="location.reload()"> Reload </button>`;
         }              
     }
 });
